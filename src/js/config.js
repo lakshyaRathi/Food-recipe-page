f17beeb233d13cd30c0b5a1ca677071b74ad7e7d
lakshyaRathi <rathilakshya2001@gmail.com>
2023-04-02 22:22:49 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f158575c-d5c3-4131-a7b7-8ce257200fb9';
+export const MODAL_CLOSE_SEC = 2.5;
